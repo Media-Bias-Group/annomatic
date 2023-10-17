faebc801119baf083497f59e49b1a0cc6851bb4d
Christoph Mandl <mandlc@fim.uni-passau.de>
2023-10-17 21:23:34 +0200

    Add LabelTemplateSegment to Prompt

--- annomatic/prompt/segment.py
+++ annomatic/prompt/segment.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+
 from typing import Any, List
 
 from annomatic.prompt.base import FstringTemplater
@@ -56,7 +57,7 @@ class PromptPlainSegment(PromptSegment):
         return self._content
 
 
-class PromptTemplateSegment(FstringTemplater, PromptSegment):
+class PromptTemplateSegment(PromptSegment):
     """
     This is class represents a part of a Prompt that has his own
     sub-template.
@@ -66,11 +67,57 @@ class PromptTemplateSegment(FstringTemplater, PromptSegment):
 
     """
 
-    def __init__(self, content: str = ""):
-        super().__init__(template=content)
+    def __init__(self, template: str = ""):
+        super().__init__()
+        self._template = FstringTemplater(template)
 
     def get_variables(self) -> List[str]:
-        return super().get_variables()
+        return self._template.get_variables()
 
     def to_string(self, **kwargs: Any) -> str:
-        return super().parse(**kwargs)
+        return self._template.parse(**kwargs)
+
+
+class LabelTemplateSegment(PromptSegment):
+    """
+    This is class represents a part of a Prompt that has his own
+    sub-template.
+
+    This class directly uses the F-String Templater and uses {..}
+    to determine attributes.
+    """
+
+    SEPARATOR = ", "
+    """Separator between labels"""
+
+    LAST_SEPARATOR = " or "
+    """Separator between last two labels"""
+
+    def __init__(self, template: str, label_var: str = "labels"):
+        super().__init__()
+        self._template = FstringTemplater(template)
+        self._label_var = label_var
+
+    def get_variables(self) -> List[str]:
+        return self._template.get_variables()
+
+    def to_string(self, **kwargs: Any) -> str:
+        if self._label_var in kwargs:
+            labels = kwargs[self._label_var]
+            if isinstance(labels, list) and labels:
+                if len(labels) == 1:
+                    kwargs[self._label_var] = labels[0]
+                    return self._template.parse(**kwargs)
+                else:
+                    label_string = self.SEPARATOR.join(labels[:-1])
+                    label_string += self.LAST_SEPARATOR + labels[-1]
+                    kwargs[self._label_var] = label_string
+                    return self._template.parse(**kwargs)
+            elif isinstance(labels, str):
+                return self._template.parse(**kwargs)
+            else:
+                raise ValueError(
+                    f"Invalid value for labels variable: {self._label_var}",
+                )
+        else:
+            return self._template.parse(**kwargs)
--- tests/prompt/test_prompt.py
+++ tests/prompt/test_prompt.py
@@ -69,11 +69,53 @@ def test_basic_prompt_multiple_segments():
     )
 
     prompt = Prompt()
-    prompt.add_Part(content=template_input)
-    prompt.add_Part(content=template_task)
-    prompt.add_Part(content=template_out)
+    prompt.add_part(content=template_input)
+    prompt.add_part(content=template_task)
+    prompt.add_part(content=template_out)
 
     res = prompt.to_string(input=input)
+    assert result == res
+
+
+def test_basic_prompt_multiple_inputs():
+    result = (
+        "Instruction: 'Since then, health care has turned out "
+        "to be a very strong issue for Democrats, "
+        "who campaigned on the issue aggressively during the "
+        "2018 midterms and enjoyed a net"
+        " gain of 40 seats in the U.S. House of Representatives.'"
+        "\n\n"
+        "Classify the sentence above as PERSUASIVE TECHNIQUES "
+        "or NO PERSUASIVE TECHNIQUES."
+        "\n\n"
+        "Output: "
+    )
+
+    template_input = "Instruction: '{input}'"
+
+    template_task = "Classify the sentence above as {labels}."
+    template_out = "{pred}: "
+
+    input = (
+        "Since then, health care has turned out to be a "
+        "very strong issue for Democrats, who campaigned on the issue "
+        "aggressively during the 2018 midterms and enjoyed "
+        "a net gain of 40 seats in the U.S. House of Representatives."
+    )
+
+    prompt = Prompt()
+    prompt.add_part(content=template_input)
+    prompt.add_labels_part(content=template_task)
+    prompt.add_part(content=template_out)
+
+    res = prompt.to_string(
+        input=input,
+        labels=[
+            "PERSUASIVE TECHNIQUES",
+            "NO PERSUASIVE TECHNIQUES",
+        ],
+        pred="Output",
+    )
     assert result == res
 
 
@@ -83,8 +125,8 @@ def test_get_variable():
     template_out = "Instruction: '{output}'"
 
     prompt = Prompt()
-    prompt.add_Part(content=template_input)
-    prompt.add_Part(content=template_out)
+    prompt.add_part(content=template_input)
+    prompt.add_part(content=template_out)
 
     res = prompt.get_variables()
     assert res == ["input", "output"]
