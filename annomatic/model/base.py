c7f1b60438a240dc9740889bf33d9e2f3c7f8038
Christoph Mandl <mandlc@fim.uni-passau.de>
2023-10-23 18:11:25 +0200

    Added HuggingFace models that are available with AutoModelForCausalLM

--- annomatic/model/base.py
+++ annomatic/model/base.py
@@ -1,25 +1,69 @@
 from abc import ABC, abstractmethod
+
 from typing import Any
 
 
 class Response:
     """
-    Base class for the Answer given by LLMs.
+    Class for the Response given by LLMs.
 
     Arguments:
         answer: the parsed answer
-        _data: is the raw unedited output produced model
+        _data: is the raw unedited output produced by the model
     """
 
     def __init__(self, answer: str, data: Any):
         self.answer = answer
         self._data = data
 
+    def __str__(self):
+        return self.answer
+
+
+class ResponseList:
+    """
+    Class for the list of Responses given by LLMs.
+
+    Arguments:
+        answers: list of parsed answers
+        data: list of raw unedited outputs produced by the model
+    """
+
+    def __init__(self, answers=None, data=None):
+        if answers is None:
+            answers = []
+        if data is None:
+            data = []
+        if len(answers) != len(data):
+            raise ValueError(
+                "The length of 'answers' and 'data' lists must be the same.",
+            )
+        self.responses = [
+            Response(answer, data_point)
+            for answer, data_point in zip(answers, data)
+        ]
+
+    def __len__(self):
+        return len(self.responses)
+
+    def __getitem__(self, index):
+        return self.responses[index]
+
+    def __iter__(self):
+        return iter(self.responses)
+
+    def __str__(self):
+        return "\n".join([str(response) for response in self.responses])
+
+    def __repr__(self):
+        return f"ResponseList({self.responses})"
+
 
 class Model(ABC):
     """
     Base Model for LLMs
     """
+
     @abstractmethod
     def predict(self, message):
         """
---
+++ annomatic/model/huggingface/huggingface.py
@@ -0,0 +1,119 @@
+from abc import ABC
+
+from typing import List
+
+from annomatic.model.base import ResponseList
+
+try:
+    from transformers import AutoModelForCausalLM, AutoTokenizer
+except ImportError as e:
+    raise ValueError(
+        'Install "poetry install --with huggingface" before using this model!',
+        e,
+    ) from None
+
+
+class HuggingFaceModel(ABC):
+    """
+    Base class for all HuggingFace models.
+    """
+
+    def __init__(self, model_name: str, token_args=None):
+        if token_args is None:
+            token_args = {}
+        if token_args is None:
+            token_args = {}
+        self.tokenizer = AutoTokenizer.from_pretrained(
+            model_name,
+            **token_args,
+        )
+        self.model = None
+
+    def predict(self, messages: List[str]) -> ResponseList:
+        """
+        Predicts the response for the given list of messages.
+
+        Args:
+            messages: List of string messages to predict the response for.
+
+        Returns:
+            The predicted responses as a ResponseList.
+        """
+        if self.model is None:
+            raise ValueError(
+                "Model is not initialized!",
+            )
+
+        if isinstance(messages, List) and len(messages) > 1:
+            padding = True
+            if not self.tokenizer.pad_token:
+                print(
+                    "Tokenizer doesn't have a pad_token! Use pad_token_id = 0",
+                )
+                self.tokenizer.pad_token_id = 0
+        else:
+            padding = False
+
+        model_inputs = self.tokenizer(
+            messages,
+            padding=padding,
+            return_tensors="pt",
+            return_length=True,
+        )
+        # Pop length from model_inputs, otherwise set length to default (20)
+        input_length = int(model_inputs.pop("length", 20).max())
+        if self.model is not None and self.model.device.type == "cuda":
+            model_inputs = model_inputs.to("cuda")
+
+        # TODO reasonable/dynamic max_length
+        model_outputs = self.model.generate(
+            **model_inputs,
+            max_length=input_length * 2,
+        )
+
+        decoded_output = self.tokenizer.batch_decode(
+            model_outputs,
+            skip_special_tokens=True,
+        )
+
+        # remove the input from any response
+        responses = [
+            response[len(prefix) :].strip()
+            for response, prefix in zip(decoded_output, messages)
+        ]
+
+        return ResponseList(responses, decoded_output)
+
+
+class HFAutoModelForCausalLM(HuggingFaceModel):
+    """
+    A model that uses the AutoModelForCausalLM class from the HuggingFace
+    transformers library.
+
+    This model uses models that are available for the
+    AutoModelForCausalLM class.
+
+    See
+    https://huggingface.co/transformers/v3.5.1/model_doc/auto.html#tfautomodelforcausallm
+    for details.
+    """
+
+    def __init__(
+        self,
+        model_name: str,
+        model_args=None,
+        token_args=None,
+    ):
+        super().__init__(model_name=model_name, token_args=token_args)
+
+        if model_args is None:
+            model_args = {}
+
+        if not any(arg in model_args for arg in ["device", "device_map"]):
+            print("No device option defined. Set 'device_map'='auto'")
+            model_args["device_map"] = "auto"
+
+        self.model = AutoModelForCausalLM.from_pretrained(
+            model_name,
+            **model_args,
+        )
