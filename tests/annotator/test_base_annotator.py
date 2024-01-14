7d7000ed912253b20ee17ae640f5ab859b05a2fa
Christoph Mandl <mandlc@fim.uni-passau.de>
2024-01-14 16:26:14 +0100

    introduce unittest lib for csv annotator tests

--- tests/annotator/test_base_annotator.py
+++ tests/annotator/test_base_annotator.py
@@ -2,6 +2,7 @@ import unittest
 from unittest.mock import MagicMock, patch
 
 import pandas as pd
+import pytest
 
 from annomatic.annotator import HuggingFaceCsvAnnotator
 from annomatic.prompt import Prompt
@@ -20,7 +21,7 @@ class YourTestClass(unittest.TestCase):
             return_value=MagicMock(),
         ):
             annotator = HuggingFaceCsvAnnotator(
-                model_name="model",
+                model_name="mock",
                 out_path="./tests/data/output.csv",
                 labels=["BIASED", "NOT BIASED"],
             )
@@ -50,7 +51,7 @@ class YourTestClass(unittest.TestCase):
             return_value=MagicMock(),
         ):
             annotator = HuggingFaceCsvAnnotator(
-                model_name="model",
+                model_name="mock",
                 out_path="./tests/data/output.csv",
                 labels=["BIASED", "NOT BIASED"],
             )
@@ -101,7 +102,7 @@ class YourTestClass(unittest.TestCase):
             return_value=MagicMock(),
         ):
             annotator = HuggingFaceCsvAnnotator(
-                model_name="model",
+                model_name="mock",
                 out_path="./tests/data/output.csv",
                 labels=["BIASED", "NOT BIASED"],
             )
@@ -144,3 +145,235 @@ class YourTestClass(unittest.TestCase):
                 "Classify the sentence above as BIASED or NOT BIASED."
                 "\n\nOutput: ",
             ]
+
+    def test_set_data_csv(self):
+        with patch.object(
+            HuggingFaceCsvAnnotator,
+            "_load_model",
+            return_value=MagicMock(),
+        ):
+            annotator = HuggingFaceCsvAnnotator(
+                model_name="mock",
+                out_path="./tests/data/output.csv",
+            )
+            annotator.set_data(
+                data="./tests/data/input.csv",
+                data_variable="input",
+            )
+            assert isinstance(annotator.data, pd.DataFrame)
+
+    def test_set_data_prompt_matching(self):
+        with patch.object(
+            HuggingFaceCsvAnnotator,
+            "_load_model",
+            return_value=MagicMock(),
+        ):
+            annotator = HuggingFaceCsvAnnotator(
+                model_name="mock",
+                out_path="./tests/data/output.csv",
+            )
+
+            template = (
+                "Instruction: '{input}'"
+                "\n\n"
+                "Classify the sentence above as PERSUASIVE TECHNIQUES "
+                "or NO PERSUASIVE TECHNIQUES or as {extra}."
+                "\n\n"
+                "Output: "
+            )
+            annotator.set_prompt(prompt=template)
+            df = pd.read_csv(
+                "./tests/data/input.csv",
+            )
+            annotator.set_data(
+                data=df,
+                data_variable="input",
+            )
+
+            assert annotator.data_variable == "input"
+
+    def test_set_data_prompt_raise_value_error(self):
+        with patch.object(
+            HuggingFaceCsvAnnotator,
+            "_load_model",
+            return_value=MagicMock(),
+        ):
+            annotator = HuggingFaceCsvAnnotator(
+                model_name="mock",
+                out_path="./tests/data/output.csv",
+            )
+
+            template = (
+                "Instruction: '{input}'"
+                "\n\n"
+                "Classify the sentence above as PERSUASIVE TECHNIQUES "
+                "or NO PERSUASIVE TECHNIQUES or as {extra}."
+                "\n\n"
+                "Output: "
+            )
+            annotator.set_prompt(prompt=template)
+            df = pd.read_csv(
+                "./tests/data/input.csv",
+            )
+            with pytest.raises(ValueError) as e_info:
+                annotator.set_data(
+                    data=df,
+                    data_variable="?",
+                )
+                raise e_info
+
+    def test_set_prompt_str(self):
+        with patch.object(
+            HuggingFaceCsvAnnotator,
+            "_load_model",
+            return_value=MagicMock(),
+        ):
+            annotator = HuggingFaceCsvAnnotator(
+                model_name="mock",
+                out_path="./tests/data/output.csv",
+            )
+
+            template = (
+                "Instruction: '{input}'"
+                "\n\n"
+                "Classify the sentence above as PERSUASIVE TECHNIQUES "
+                "or NO PERSUASIVE TECHNIQUES or as {extra}."
+                "\n\n"
+                "Output: "
+            )
+            annotator.set_prompt(prompt=template)
+        assert isinstance(annotator._prompt, Prompt)
+
+    def test_set_prompt_prompt(self):
+        with patch.object(
+            HuggingFaceCsvAnnotator,
+            "_load_model",
+            return_value=MagicMock(),
+        ):
+            annotator = HuggingFaceCsvAnnotator(
+                model_name="mock",
+                out_path="./tests/data/output.csv",
+            )
+            template = (
+                "Instruction: '{input}'"
+                "\n\n"
+                "Classify the sentence above as PERSUASIVE TECHNIQUES "
+                "or NO PERSUASIVE TECHNIQUES or as {extra}."
+                "\n\n"
+                "Output: "
+            )
+            prompt = Prompt(content=template)
+            annotator.set_prompt(prompt=prompt)
+        assert isinstance(annotator._prompt, Prompt)
+
+    def test_soft_parse(self):
+        with patch.object(
+            HuggingFaceCsvAnnotator,
+            "_load_model",
+            return_value=MagicMock(),
+        ):
+            df = pd.DataFrame(
+                {
+                    "response": [
+                        "This is a biased response.",
+                        "A not biased response.",
+                        "Another biased statement.",
+                        "This sentence is not parseable.",
+                        "This is a biased response or Not biased.",
+                    ],
+                },
+            )
+
+            annotator = HuggingFaceCsvAnnotator(
+                model_name="mock",
+                out_path="./tests/data/output.csv",
+            )
+            annotator._labels = ["BIASED", "NOT BIASED"]
+            annotator._soft_parse(df, "response", "label")
+
+            assert df["label"].iloc[0] == "BIASED"
+            assert df["label"].iloc[1] == "NOT BIASED"
+            assert df["label"].iloc[2] == "BIASED"
+            assert df["label"].iloc[3] == "?"
+            assert df["label"].iloc[4] == "?"
+
+    def test_validate_labels_only_init(self):
+        with patch.object(
+            HuggingFaceCsvAnnotator,
+            "_load_model",
+            return_value=MagicMock(),
+        ):
+            annotator = HuggingFaceCsvAnnotator(
+                model_name="mock",
+                out_path="./tests/data/output.csv",
+                labels=["BIASED", "NON-BIASED"],
+            )
+            annotator._prompt = Prompt("This is a prompt")
+            annotator._validate_labels()
+
+        assert annotator._labels == ["BIASED", "NON-BIASED"]
+
+    def test_validate_labels_only_prompt(self):
+        with patch.object(
+            HuggingFaceCsvAnnotator,
+            "_load_model",
+            return_value=MagicMock(),
+        ):
+            annotator = HuggingFaceCsvAnnotator(
+                model_name="mock",
+                out_path="./tests/data/output.csv",
+            )
+
+            prompt = Prompt()
+            prompt.add_labels_part(
+                content="This is a prompt {labels}",
+                label_var="labels",
+            )
+            annotator._prompt = prompt
+            annotator._validate_labels(labels=["BIASED", "NON-BIASED"])
+
+        assert annotator._labels == ["BIASED", "NON-BIASED"]
+
+    def test_validate_labels_matching(self):
+        with patch.object(
+            HuggingFaceCsvAnnotator,
+            "_load_model",
+            return_value=MagicMock(),
+        ):
+            annotator = HuggingFaceCsvAnnotator(
+                model_name="mock",
+                out_path="./tests/data/output.csv",
+                labels=["BIASED", "NON-BIASED"],
+            )
+
+            prompt = Prompt()
+            prompt.add_labels_part(
+                content="This is a prompt {label}",
+                label_var="label",
+            )
+            annotator._prompt = prompt
+            annotator._validate_labels(label=["BIASED", "NON-BIASED"])
+
+        assert annotator._labels == ["BIASED", "NON-BIASED"]
+
+    def test_validate_labels_not_matching(self):
+        with patch.object(
+            HuggingFaceCsvAnnotator,
+            "_load_model",
+            return_value=MagicMock(),
+        ):
+            annotator = HuggingFaceCsvAnnotator(
+                model_name="mock",
+                out_path="./tests/data/output.csv",
+                labels=["BIASED", "NON-BIASED"],
+            )
+
+            prompt = Prompt()
+            prompt.add_labels_part(
+                content="This is a prompt {label}",
+                label_var="label",
+            )
+            annotator._prompt = prompt
+
+        with pytest.raises(Exception):
+            annotator._validate_labels(label=["BIASED", "Other Label"])
