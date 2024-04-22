3c9d55ae724d94fbae72331df4359cb0e67fd353
Christoph Mandl <mandlc@fim.uni-passau.de>
2024-04-22 11:00:41 +0200

    update examples

--- examples/few-shot.py
+++ examples/few-shot.py
@@ -1,34 +1,40 @@
 import pandas as pd
+from haystack.components.builders import PromptBuilder
 from haystack.components.generators import HuggingFaceLocalGenerator
 
 from annomatic.annotator import FileAnnotator
-from annomatic.prompt import Prompt
 
 # provide 2 examples of biased and not biased sentences
-data = {
-    "text": [
-        "Recent studies suggest that the new technology is revolutionary.",
-        "Critics argue that the government's policies are misguided "
-        "and harmful.",
-    ],
-    "label": [
-        "NOT BIASED",
-        "BIASED",
-    ],
-}
-df_examples = pd.DataFrame(data, columns=["text", "label"])
+df_examples = pd.DataFrame(
+    {
+        "text": [
+            "Recent studies suggest that the new technology is revolutionary.",
+            "Critics argue that the government's policies are misguided "
+            "and harmful.",
+        ],
+        "label": [
+            "NOT BIASED",
+            "BIASED",
+        ],
+    },
+)
 
 # sentence to be annotated
 query = ["The new technology is revolutionary."]
 df = pd.DataFrame({"text": query})
 
-# create a Prompt object (with f-string template)
-prompt = Prompt()
-prompt.add_part("Instruction: '{text}'")
-prompt.add_labels_part("Classify the sentence above as {label}.")
-prompt.add_part("Output: ")
+prompt = PromptBuilder(
+    "{% for key,value in examples.iterrows() %}"
+    "Instruction: '{{value.text}}'\n"
+    "Classify the sentence above as BIASED or NOT BIASED.\n"
+    "Output: {{ value.label }}\n\n"
+    "{% endfor %}"
+    "Instruction: '{{text}}'\n"
+    "Classify the sentence above as BIASED or NOT BIASED.\n"
+    "Output: ",
+)
 
-# create model via Haystack 2.0
+# create Haystack 2.0 model
 model = HuggingFaceLocalGenerator(
     task="text2text-generation",
     generation_kwargs={"max_new_tokens": 100, "temperature": 0.9},
@@ -39,14 +45,16 @@ annotator = FileAnnotator(
     model=model,
     out_path="./output.csv",
     out_format="csv",
+    labels=["BIASED", "NOT BIASED"],
 )
 
 # set data and prompt
 annotator.set_data(df, data_variable="text")
 annotator.set_prompt(prompt)
 
-annotator.set_context(df_examples)
+# map the examples to the prompt value for examples
+annotator.set_context({"examples": df_examples})
 
 # annotate the data and return as df
-result = annotator.annotate(label=["BIASED", "NOT BIASED"], return_df=True)
+result = annotator.annotate(return_df=True)
 print(result)
